342d40ace52501c0dbddc50a9b37250cf2673f6b
forsythrl <tarrantrl@utexas.edu>
2020-09-07 15:10:35 -0500

    add five day forecast fetch and render

--- assets/js/script.js
+++ assets/js/script.js
@@ -8,6 +8,8 @@ var searchContainerEl = document.querySelector("#search-container");
 var previousSearchesEl = document.querySelector("#previous-searches");
 // select the main results container
 var mainResultsEl = document.querySelector("#main-results");
+// select five day results container
+var fiveDayResultsEl = document.querySelector("#five-day-results");
 
 // create list to store searched cities
 var cities = [];
@@ -34,13 +36,7 @@ var cityFetch = function(city){
             if (!cities.includes(city)){
                 // add city to list
                 cities.push(city);
-                // create button element
-                var cityBtn = document.createElement("button");
-                cityBtn.classList = "city-btn";
-                cityBtn.setAttribute("data-city-name", city);
-                cityBtn.textContent = city;
-                // append button to search container
-                previousSearchesEl.appendChild(cityBtn);
+                addCityBtn(city);
                 // save list to local storage
                 localStorage.setItem("cities", JSON.stringify(cities));
             }
@@ -110,16 +106,12 @@ var cityFetch = function(city){
                             var uvSpan = document.createElement("span");
                             uvSpan.textContent = uv;
                             // determine uv index color
-                            if (uv < 3){
+                            if (uv < 4){
                                 uvSpan.classList = "uv-low";
-                            }else if (uv < 6){
+                            }else if (uv < 7){
                                 uvSpan.classList = "uv-mod";
-                            }else if (uv < 8){
-                                uvSpan.classList = "uv-high";
-                            }else if (uv < 11){
-                                uvSpan.classList = "uv-very-high";
                             }else{
-                                uvSpan.classList = "uv-extreme";
+                                uvSpan.classList = "uv-high";
                             }
                             // append span to li
                             uvLi.appendChild(uvSpan);
@@ -130,6 +122,8 @@ var cityFetch = function(city){
                 })
                 // append ul to main results
                 mainResultsEl.appendChild(weatherList);
+                // get 5 day results
+                fiveDayFetch(city);
             })
         } else{
             alert("Error: " + response.statusText);
@@ -137,12 +131,73 @@ var cityFetch = function(city){
     })  
 }
 
+// function to fetch 5 day forecast
+var fiveDayFetch = function(city){
+    fetch(`http://api.openweathermap.org/data/2.5/forecast?q=${city}&appid=f4f14a83552976dedb188c08b5e2b54b`).then(function(response){
+        if (response.ok){
+            // convert to json
+            response.json().then(function(data){
+                var currentDate = moment().format("MM/DD/YYYY");
+                //iterate through list
+                for (var i = 0; i < data.list.length; i++){
+                    var uDate = data.list[i].dt;
+                    var date = moment.unix(uDate).format("MM/DD/YYYY");
+                    // check if the date is the same date. If not, create a card for the screen
+                    if (date != currentDate){
+                        renderFiveDay(data.list[i]);
+                        currentDate = date;
+                    }
+                    
+                }
+            })
+        }
+    })
+}
+
+var renderFiveDay = function(day){
+    // get date
+    var date = moment.unix(day.dt).format("MM/DD/YYYY");
+    // get icon
+    var icon = day.weather[0].icon;
+    // icon url
+    var iconUrl = `http://openweathermap.org/img/wn/${icon}@2x.png`;
+    // create icon img element
+    var iconImg = document.createElement("img");
+    iconImg.setAttribute("src", iconUrl);
+    // get temp in kelvin
+    var temp = day.main.temp;
+    // convert temp to f
+    var fTemp = ((temp - 273.15) * (9/5) + 32).toFixed(1);
+    // get humidity
+    var humidity = day.main.humidity;
+    // create a card for the forecast data
+    var forecastCard = document.createElement("div");
+    forecastCard.classList = "col-sm-4 col-md-2 card";
+    // create header
+    var forecastHead = document.createElement("h5");
+    forecastHead.textContent = date;
+    // append header to card
+    forecastCard.appendChild(forecastHead);
+    // append icon
+    forecastCard.appendChild(iconImg);
+    // create p tags for temp and humidity
+    var tempEl = document.createElement("p");
+    tempEl.textContent = `Temp: ${temp} °F`;
+    var humidityEl = document.createElement("p");
+    humidityEl.textContent = `Humidity: ${humidity}%`;
+    // append to forecast card
+    forecastCard.appendChild(tempEl);
+    forecastCard.appendChild(humidityEl);
+    // append to five day div
+    fiveDayResultsEl.appendChild(forecastCard);
+}
+
 // function to render previous searches
 var loadCities = function(){
     // set cities variable to localStorage value
     cities = JSON.parse(localStorage.getItem("cities"));
-    console.log(cities);
-    console.log("text");
+    //console.log(cities);
+    //console.log("text");
     if (!cities){
         cities = [];
         return
@@ -150,15 +205,22 @@ var loadCities = function(){
     // render each city on the page as a button
     for (var i = 0; i < cities.length; i++){
         // create button element
-        var cityBtn = document.createElement("button");
-        cityBtn.classList = "city-btn";
-        cityBtn.setAttribute("data-city-name", cities[i]);
-        cityBtn.textContent = cities[i];
-        // append button to search container
-        previousSearchesEl.appendChild(cityBtn);
+        addCityBtn(cities[i])
     }
 }
 
+// function to add previously searched city as a button
+var addCityBtn = function(city){
+    // create button element
+    var cityBtn = document.createElement("button");
+    cityBtn.classList = "city-btn";
+    cityBtn.setAttribute("data-city-name", city);
+    cityBtn.textContent = city;
+    // append button to search container
+    previousSearchesEl.appendChild(cityBtn);
+}
+
+// function to handle when a previously searched city button is clicked
 var prevSearchHandler = function(event){
     // get the city name from the closest button clicked
     var city = event.target.closest(".city-btn").textContent;
